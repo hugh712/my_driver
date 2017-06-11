2b5d1caddca535e1b165a5fb1a259e98fd0d71e5
hugh <hugh@example.com>
2017-06-11 13:22:22 +0800

    update

--- 009_kobject/kobj.c
+++ 009_kobject/kobj.c
@@ -17,13 +17,8 @@ ssize_t kobject_store(struct kobject *kobject, struct attribute *attr,const char
 
 struct kobject kobj_Parent;
 struct kobject kobj1;
-struct kobject *kobj2;
-struct kobject *kobj3;
+struct kobject kobj2;
 struct kset *kset1;
-struct kset kset2;
-
-
-//for kset
 
 static int mykset_uevent_filter(struct kset *kset, struct kobject *kobj)
 {
@@ -35,8 +30,6 @@ static int mykset_uevent_filter(struct kset *kset, struct kobject *kobj)
 static const struct kset_uevent_ops mykset_uevent_ops = {
 	.filter = mykset_uevent_filter,
 };
-
-
 
 //for attribute
 struct attribute attr1 = {
@@ -52,7 +45,6 @@ struct attribute attr3 = {
 	.mode = S_IRWXUGO,
 };
 
-//for "default attribute" -> attribute
 static struct attribute *def_attrs1[]={
 	&attr1,
 	NULL,
@@ -65,7 +57,6 @@ static struct attribute *def_attrs3[]={
 	&attr3,
 	NULL,
 };
-
 
 static int hugh_bus_match(struct device *_dev, struct device_driver *_drv)
 {
@@ -102,10 +93,6 @@ static void hugh_bus_shutdown(struct device *dev)
 	PTK("call bus shutdown");
 }
 
-/*
-*    for bus
-*		 for bus
-*/
 struct bus_type mytest_bus_type = {
 	.name		= "hugh_bus",
 	.match		= hugh_bus_match,
@@ -116,18 +103,11 @@ struct bus_type mytest_bus_type = {
 	.shutdown	= hugh_bus_shutdown,
 };
 
-/*
-*			for k object
-*			for k object
-*/
-
-//for sysfs file operation
 struct sysfs_ops obj_sysops={
 	.show = kobject_show,
 	.store = kobject_store,
 };
 
-//for kernel object type
 struct kobj_type ktype1={
 	.release=kobject_release,
 	.sysfs_ops=&obj_sysops,
@@ -146,43 +126,40 @@ struct kobj_type ktype3={
 
 static int __init hello_init(void)
 {
-
 	PTK("bus register");
 	bus_register(&mytest_bus_type);	
+
+	PTK("kobject-parent init");
+	kobject_init_and_add(&kobj_Parent, &ktype1, NULL, "kobject-Parent");
 
 	PTK("kset1 register");
 	kset1=kset_create_and_add("kset1",&mykset_uevent_ops,&kobj_Parent);
 
 	PTK("kobject-1 init");
 	kobject_init_and_add(&kobj1, &ktype1, NULL, "kobject-1");
-	kobj1.kset = kset1;	
+	kobj1.parent = &kobj_Parent;	
 
-/*
 	PTK("kobject-2 init");
-	kobject_init_and_add(kobj2, &ktype2, NULL, "kobject-2");
-	
-	PTK("kobject-3 init");
-	kobject_init_and_add(kobj3, &ktype3, NULL, "kobject-3");
-*/
+	kobject_init_and_add(&kobj2, &ktype2, NULL, "kobject-2");
+	kobj2.kset=kset1;
+
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-
 	PTK("kset - 1 exit");
 	kset_unregister(kset1);	
+
+	PTK("kobject - Parent exit");
+	kobject_del(&kobj_Parent);
 
 	PTK("kobject - 1 exit");
 	kobject_del(&kobj1);
 
-/*	
 	PTK("kobject - 2 exit");
-	kobject_del(kobj2);
+	kobject_del(&kobj2);
 
-	PTK("kobject - 3 exit");
-	kobject_del(kobj3);
-*/
 	PTK("mytest_bus exit");
 	bus_unregister(&mytest_bus_type);
 }
